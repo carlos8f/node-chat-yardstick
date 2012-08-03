22217403da4d2fd7c7dd860ebdb302da5ff16bd1
Carlos Rodriguez <carlos@s8f.org>
2012-08-03 13:49:58 -0700

    engine.io implementation

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var http = require('http')
-  , socketio = require('socket.io')
+  , engine = require('engine.io')
   , argv = require('optimist')
     .alias('p', 'port')
     .default('port', 3000)
     .argv
-  , buffet = require('buffet')(require('path').resolve(__dirname, './public'))
+  , fs = require('fs')
+  , path = require('path')
+  , pub = path.resolve(__dirname, './public')
+  , EventEmitter = require('events').EventEmitter
   ;
+
+fs.writeFileSync(path.join(pub, 'engine.io.js'), fs.readFileSync(path.resolve(__dirname, './node_modules/engine.io-client/dist/engine.io.js')));
+var buffet = require('buffet')(pub);
 
 var server = http.createServer(function(req, res) {
   buffet(req, res, buffet.notFound.bind(null, req, res));
@@ -13,44 +19,61 @@ var server = http.createServer(function(req, res) {
   console.log('test server running on port ' + argv.port);
 });
 
-var io = socketio.listen(server, {
-  transports: ['xhr-polling']
-});
+var io = engine.attach(server);
 
-io.configure(function() {
-  io.set('log level', 2); // info
-  io.set('polling duration', 45);
-  io.set('close timeout', 55);
-});
-io.configure('production', function() {
-  io.set('log level', 0); // error
-});
-
+io.emitAll = function(name) {
+  var args = Array.prototype.slice.call(arguments);
+  Object.keys(io.clients).forEach(function(id) {
+    io.clients[id].sendData.apply(io.clients[id], args);
+  });
+};
 var nicknames = {};
 
-io.sockets.on('connection', function (socket) {
-  socket.on('user message', function (msg) {
-    socket.broadcast.emit('user message', socket.nickname, msg);
+io.on('connection', function(socket) {
+  socket.sendData = function(name) {
+    data = Array.prototype.slice.call(arguments, 1);
+    socket.send(JSON.stringify({name: name, data: data}));
+  };
+  socket.broadcast = function(name) {
+    var args = Array.prototype.slice.call(arguments);
+    Object.keys(io.clients).forEach(function(id) {
+      if (id === socket.id) return;
+      io.clients[id].sendData.apply(io.clients[id], args);
+    });
+  };
+  socket.on('message', function(msg) {
+    var unpacked = JSON.parse(msg);
+    if (unpacked.name) {
+      socket.emit.apply(socket, [unpacked.name].concat(unpacked.data));
+    }
+  });
+  socket.on('user message', function(msg) {
+    socket.broadcast('user message', socket.nickname, msg);
   });
 
-  socket.on('nickname', function (nick, fn) {
+  socket.on('nickname', function(nick) {
     if (!nick || nick === '' || nicknames[nick]) {
-      fn(true);
-    } else {
-      fn(false);
+      socket.sendData('nickname', true);
+    }
+    else {
+      socket.sendData('nickname', false);
       nicknames[nick] = socket.nickname = nick;
-      socket.broadcast.emit('announcement', nick + ' connected');
+      socket.broadcast('announcement', nick + ' connected');
       var names = Object.keys(nicknames);
       names.sort();
-      io.sockets.emit('nicknames', names);
+      io.emitAll('nicknames', names);
     }
   });
 
-  socket.on('disconnect', function () {
+  socket.on('close', function () {
     if (!socket.nickname) return;
 
     delete nicknames[socket.nickname];
-    socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
-    socket.broadcast.emit('nicknames', nicknames);
+    socket.broadcast('announcement', socket.nickname + ' disconnected');
+    io.emitAll('nicknames', nicknames);
+  });
+
+  socket.on('error', function(err) {
+    console.error(err);
   });
 });
--- public/main.js
+++ public/main.js
@@ -1,40 +1,93 @@
-// socket.io specific code
-var socket = io.connect();
-
-socket.on('connect', function () {
-  $('#chat').addClass('connected');
-});
-
-socket.on('announcement', function (msg) {
+function Client(options) {
+  options || (options = {});
+  this.reconnectTimeout = this.activeReconnectTimeout = options.reconnectTimeout || 1000;
+  this.reconnectBackoff = options.reconnectBackoff || 1.7;
+  this.reconnectAttempts = 0;
+  this.port = options.port;
+  this.connected = false;
+  this.connect();
+}
+Client.prototype.connect = function() {
+  this.socket = new eio.Socket({port: this.port});
+  var self = this;
+  this.socket.on('open', function() {
+    self.connected = true;
+    this.reconnectAttempts = 0;
+    message('System', 'Connected to the server');
+    self.activeReconnectTimeout = self.reconnectTimeout;
+  });
+  this.socket.on('close', function(reason, desc) {
+    if (self.connected) {
+      message('System', 'Disconnected from the server');
+    }
+    self.connected = false;
+    if (reason !== 'forced close') {
+      self.activeReconnectTimeout *= self.reconnectBackoff;
+      setTimeout(function() {
+        self.onReconnecting();
+        self.connect();
+      }, self.activeReconnectTimeout);
+    }
+  });
+  this.socket.on('error', function(err) {
+    if (self.connected) {
+      message('System', err.message ? err.message : 'A unknown error occurred');
+    }
+  });
+  this.socket.on('message', function(msg) {
+    var unpacked = JSON.parse(msg), func;
+    switch (unpacked.name) {
+      case 'announcement': func = self.onAnnouncement; break;
+      case 'nicknames': func = self.onNicknames; break;
+      case 'nickname': func = self.onNickname; break;
+      case 'user message': func = self.onUserMessage; break;
+      default: return;
+    }
+    func.apply(self, unpacked.data);
+  });
+};
+Client.prototype.onAnnouncement = function(msg) {
   $('#lines').append($('<p>').append($('<em>').text(msg)));
-});
-
-socket.on('nicknames', function (nicknames) {
+};
+Client.prototype.onNicknames = function(nicknames) {
   $('#nicknames').empty().append($('<h3>Online</h3><br>'));
   for (var i in nicknames) {
     $('#nicknames').append($('<b>').text(nicknames[i]), '<br>');
   }
-});
+};
+Client.prototype.onUserMessage = message;
+Client.prototype.onReconnecting = function() {
+  if (this.reconnectAttempts === 0) {
+    message('System', 'Attempting to re-connect to the server');
+  }
+  this.reconnectAttempts++;
+};
+Client.prototype.emit = function(name, data) {
+  this.socket.send(JSON.stringify({name: name, data: Array.prototype.slice.call(arguments, 1)}));
+};
+Client.prototype.onNickname = function(set) {
+  if (!set) {
+    $('#set-nickname').css('display', 'none');
+    $('#send-message').removeClass('hide');
+    $('#messages').removeClass('hide');
+    clear();
+    $('#nick-display').text(nick);
+    return;
+  }
+  $('#set-nickname fieldset').addClass('error');
+  $('#nickname-err').removeClass('hide');
+};
 
-socket.on('user message', message);
-socket.on('reconnect', function () {
-  message('System', 'Reconnected to the server');
-});
+var client = new Client({port: 3000});
 
-socket.on('reconnecting', function () {
-  message('System', 'Attempting to re-connect to the server');
-});
-
-socket.on('error', function (e) {
-  message('System', e ? e : 'A unknown error occurred');
-});
-
-function message (from, msg) {
+function message(from, msg) {
   $('#lines').append($('<p>').append($('<b>').text(from), msg));
   $('#lines').get(0).scrollTop = 10000000;
 }
+function clear() {
+  $('#message').val('').focus();
+}
 
-// dom manipulation
 $(function() {
   $('#nickname').on('hidden', function() {
     $('#set-nickname fieldset').removeClass('error');
@@ -43,29 +96,14 @@ $(function() {
   });
   $('#set-nickname').submit(function(e) {
     var nick = $('#nick').val();
-    socket.emit('nickname', nick, function(set) {
-      if (!set) {
-        $('#set-nickname').css('display', 'none');
-        $('#send-message').removeClass('hide');
-        $('#messages').removeClass('hide');
-        clear();
-        $('#nick-display').text(nick);
-        return;
-      }
-      $('#set-nickname fieldset').addClass('error');
-      $('#nickname-err').removeClass('hide');
-    });
+    client.emit('nickname', nick);
     return false;
   });
 
   $('#send-message').submit(function() {
     message('me', $('#message').val());
-    socket.emit('user message', $('#message').val());
+    client.emit('user message', $('#message').val());
     clear();
     return false;
   });
-
-  function clear() {
-    $('#message').val('').focus();
-  };
 });
